5b8dc4abfbbbcf3b2c555c5f82606b74e3de0b67
chiefkufre <samuelkufrewillie@gmail.com>
2024-07-27 07:30:26 +0100

    refactor

--- api/contact.js
+++ api/contact.js
@@ -10,16 +10,13 @@ export default async function handler(req, res) {
   if (!email || !accessToken) {
     return res.status(400).json({ message: 'Missing email or access token' });
   }
-
   const url = `${hubspotAPIUrl}${email}?idProperty=${idProperty}`;
-
   try {
     const response = await axios.get(url, {
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
     });
-
     res.status(200).json(response.data);
   } catch (error) {
     console.error(error);
