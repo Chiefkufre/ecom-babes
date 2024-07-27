6d480ce935855543b5aeeeca58755d535d7e8f48
chiefkufre <samuelkufrewillie@gmail.com>
2024-07-27 06:40:34 +0100

    server fix

--- api/contact.js
+++ api/contact.js
@@ -17,7 +17,6 @@ export default async function handler(req, res) {
     const response = await axios.get(url, {
       headers: {
         Authorization: `Bearer ${accessToken}`,
-        "Content-Type": "application/json",
       },
     });
 
