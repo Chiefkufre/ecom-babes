8873ea3ab1ea0afb57434063ac138dd8d9beaadc
chiefkufre <samuelkufrewillie@gmail.com>
2024-07-27 07:19:09 +0100

    improve endpoint

--- api/contact.js
+++ api/contact.js
@@ -20,7 +20,7 @@ export default async function handler(req, res) {
       },
     });
 
-    res.status(200).json(response.data.id);
+    res.status(200).json(response.data);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Error fetching contact data' });
